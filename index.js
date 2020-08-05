b1e1b6c8499a18420a92ae91bf8cb93d88b3fa73
Your Name <you@example.com>
2020-08-05 19:11:22 +0800

    0805

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var express = require('express');
+const express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
+
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
 app.use(express.static(__dirname+ '/public'));
 app.get('/hello', function (req, res) {
     res.send('Hello World');
@@ -12,5 +16,11 @@ app.get('/responseData', (req, res) => {
 })
 app.get('/get', (req, res) => {
     res.send(req.query);
+})
+app.post('/post', (req, res) => {
+    res.send(req.body);
+})
+app.post('/json', (req, res) => {
+    res.send(req.body);
 })
 app.listen(3000);
