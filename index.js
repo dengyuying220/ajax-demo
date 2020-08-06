fb5bee368bcd055f2b3e26582e5f55406ed58971
Your Name <you@example.com>
2020-08-06 17:38:32 +0800

    0806

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
-const express = require('express');
+const express = require("express");
 var app = express();
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
+const fs = require('fs')
 //app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ extended: true }));
-app.use(express.static(__dirname+ '/public'));
-app.get('/hello', function (req, res) {
-    res.send('Hello World');
+app.use(bodyParser.json({
+  extended: true
+}));
+app.use(express.static(__dirname + "/public"));
+app.get("/hello", function (req, res) {
+  res.send("Hello World");
 });
-app.get('/first', (req, res) => {
-    res.send('hello ajax');
-})
-app.get('/responseData', (req, res) => {
-    res.send({"name": "zhangsan"});
-})
-app.get('/get', (req, res) => {
-    res.send(req.query);
-})
-app.post('/post', (req, res) => {
-    res.send(req.body);
-})
-app.post('/json', (req, res) => {
-    res.send(req.body);
-})
+app.get("/first", (req, res) => {
+  res.send("hello ajax");
+});
+app.get("/second", (req, res) => {
+  res.send(res.query);
+  //res.send(req.body);
+});
+app.get("/responseData", (req, res) => {
+  res.send({
+    name: "zhangsan"
+  });
+});
+app.get("/get", (req, res) => {
+  res.send(req.query);
+});
+app.post("/post", (req, res) => {
+  res.send(req.body);
+});
+app.post("/json", (req, res) => {
+  res.send(req.body);
+});
+app.get("/error", (req, res) => {
+  //console.log(abc);
+  res.status(400).send("not ok");
+});
+app.get("/cache", (req, res) => {
+  fs.readFile('./test.txt', (err, result) => {
+    res.send(result);
+  })
+});
+
 app.listen(3000);
