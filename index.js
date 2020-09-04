dc4d5bd129ef2b28f206b82dc9b62aede96491a2
Your Name <you@example.com>
2020-09-04 19:35:46 +0800

    0904

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 var app = express();
 const bodyParser = require("body-parser");
 const fs = require('fs')
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json({
   extended: true
 }));
@@ -39,6 +41,27 @@ app.get("/cache", (req, res) => {
   fs.readFile('./test.txt', (err, result) => {
     res.send(result);
   })
+});
+
+app.get("/wrap", (req, res) => {
+  res.send(req.query);
+});
+
+app.post("/wrap", (req, res) => {
+  res.send(req.body);
+});
+
+app.get("/verifyEmailAddress", (req, res) => {
+  var val = req.query.email;
+  var reg = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/;
+  if (!reg.test(val)) {
+    res.send("格式错误");
+    return;
+  } else if (val === 'itheima@itcast.cn') {
+    res.status(400).send("邮箱地址已被注册");
+  } else {
+    res.send("邮箱地址可用");
+  }
 });
 
 app.listen(3000);
